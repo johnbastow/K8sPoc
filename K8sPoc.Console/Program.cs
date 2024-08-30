3f741d69631ea70f1c5fe01e50eef2c12189602f
Kerry Bastow <kbastow@arrayworks.com>
2024-08-30 16:00:03 -0400

    Added environment variables.  Cleanup.

--- K8sPoc.Console/Program.cs
+++ K8sPoc.Console/Program.cs
@@ -1,7 +1,11 @@
 ﻿var counter = 0;
 var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
+var nonSecretSetting = Environment.GetEnvironmentVariable("NON_SECRET_SETTING") ?? "[Not Set]";
+var secretSetting = Environment.GetEnvironmentVariable("SECRET_SETTING") ?? "[Not Set]";
 while (max is -1 || counter < max)
 {
     Console.WriteLine($"Hello K8s: {++counter}");
+    Console.WriteLine($"Non-Secret Setting: {nonSecretSetting}");
+    Console.WriteLine($"Secret Setting: {secretSetting}");
     await Task.Delay(TimeSpan.FromMilliseconds(1000));
 }
