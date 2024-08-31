24500cee40ebaf0843c62121f8962f20d4a09fb5
Kerry Bastow <kbastow@arrayworks.com>
2024-08-31 01:17:52 -0400

    Added Linkerd.

--- K8sPoc.ConsoleApiClient/Program.cs
+++ K8sPoc.ConsoleApiClient/Program.cs
@@ -2,19 +2,20 @@
 
 var counter = 0;
 var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
+
+//  Get Web API host name from environment variable.
 var webApiHost = Environment.GetEnvironmentVariable("WEB_API_HOST");
+
+//  Build url and create new HttpClient.
 var helloUrl = $"http://{webApiHost}/helloapi";
 var httpClient = new HttpClient();
 Console.WriteLine($"URL: {helloUrl}");
+
 while (max is -1 || counter < max)
 {
     try
     {
-        using HttpResponseMessage response = await httpClient.GetAsync(helloUrl);
-        response.EnsureSuccessStatusCode();
-        string responseBody = await response.Content.ReadAsStringAsync();
-        // Above three lines can be replaced with new helper method below
-        // string responseBody = await client.GetStringAsync(uri);
+        var responseBody = await httpClient.GetStringAsync(helloUrl);
 
         Console.WriteLine($"Hello K8s: {responseBody}");
     }
